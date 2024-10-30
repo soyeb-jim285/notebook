e26746795d9d5d4664fa59943285d0dcb86f3157
Soyeb Pervez Jim <soyeb.jim@gmail.com>
2024-10-30 23:26:28 +0600

    Added authors & generated pdf

--- content/graph/DPOnTree.h
+++ content/graph/DPOnTree.h
@@ -1,22 +1,22 @@
 /**
-* Author: Farhan
-* Description:DPonTree
-*/
+ * Author: Farhan
+ * Description:DPonTree
+ */
 vector<array<ll, 2>> down(N), up(N);
 void dfs() {
   // calculate down dp
 }
 void dfs2() {
-  ll pref = ?;
-  for (auto v: adj[u]) {
+  ll pref = ? ;
+  for (auto v : adj[u]) {
     // update up[v] and pref
   }
   reverse(adj[u].begin(), adj[u].end());
-  ll suf = ?;
-  for (auto v: adj[u]) {
+  ll suf = ? ;
+  for (auto v : adj[u]) {
     // update up[v] and suf
   }
-  for (auto v: adj[u]) {
+  for (auto v : adj[u]) {
     dfs2(v)
   }
 }
