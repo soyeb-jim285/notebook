33ddc167a580464b715627f27609e03e87598a59
Soyeb Pervez Jim <soyeb.jim@gmail.com>
2024-10-30 13:14:44 +0600

    added lazysegtree

--- content/data-structures/LazySegmentTree.h
+++ content/data-structures/LazySegmentTree.h
@@ -1,66 +1,64 @@
 /**
- * Author: Simon Lindholm
- * Date: 2016-10-08
- * License: CC0
- * Source: me
- * Description: Segment tree with ability to add or set values of large intervals, and compute max of intervals.
- * Can be changed to other things.
- * Use with a bump allocator for better performance, and SmallPtr or implicit indices to save memory.
- * Time: O(\log N).
- * Usage: Node* tr = new Node(v, 0, sz(v));
- * Status: stress-tested a bit
+ * Author: YouKnowWho
+* Description: Segment tree with lazy propagation.
+* Time: O(\log N) for upd/query, O(N \log N) for build.
+ * Usage: Node* tr = new Node(v, 0, sz(v)); 
  */
-#pragma once
 
-#include "../various/BumpAllocator.h"
-
-const int inf = 1e9;
-struct Node {
-	Node *l = 0, *r = 0;
-	int lo, hi, mset = inf, madd = 0, val = -inf;
-	Node(int lo,int hi):lo(lo),hi(hi){} // Large interval of -inf
-	Node(vi& v, int lo, int hi) : lo(lo), hi(hi) {
-		if (lo + 1 < hi) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(v, lo, mid); r = new Node(v, mid, hi);
-			val = max(l->val, r->val);
-		}
-		else val = v[lo];
-	}
-	int query(int L, int R) {
-		if (R <= lo || hi <= L) return -inf;
-		if (L <= lo && hi <= R) return val;
-		push();
-		return max(l->query(L, R), r->query(L, R));
-	}
-	void set(int L, int R, int x) {
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) mset = val = x, madd = 0;
-		else {
-			push(), l->set(L, R, x), r->set(L, R, x);
-			val = max(l->val, r->val);
-		}
-	}
-	void add(int L, int R, int x) {
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) {
-			if (mset != inf) mset += x;
-			else madd += x;
-			val += x;
-		}
-		else {
-			push(), l->add(L, R, x), r->add(L, R, x);
-			val = max(l->val, r->val);
-		}
-	}
-	void push() {
-		if (!l) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(lo, mid); r = new Node(mid, hi);
-		}
-		if (mset != inf)
-			l->set(lo,hi,mset), r->set(lo,hi,mset), mset = inf;
-		else if (madd)
-			l->add(lo,hi,madd), r->add(lo,hi,madd), madd = 0;
-	}
+const int N = 1e5 + 9;
+int a[N];
+struct segment_tree {
+#define lc (n << 1)
+#define rc ((n << 1) | 1)
+  long long t[4 * N], lazy[4 * N];
+  segment_tree() {
+    memset(t, 0, sizeof t);
+    memset(lazy, 0, sizeof lazy);
+  }
+  inline void push(int n, int b, int e) {
+    if (lazy[n] == 0)
+      return;
+    t[n] = t[n] + lazy[n] * (e - b + 1);
+    if (b != e) {
+      lazy[lc] = lazy[lc] + lazy[n];
+      lazy[rc] = lazy[rc] + lazy[n];
+    }
+    lazy[n] = 0;
+  }
+  inline long long combine(long long a, long long b) { return a + b; }
+  inline void pull(int n) { t[n] = t[lc] + t[rc]; }
+  void build(int n, int b, int e) {
+    lazy[n] = 0;
+    if (b == e) {
+      t[n] = a[b];
+      return;
+    }
+    int mid = (b + e) >> 1;
+    build(lc, b, mid);
+    build(rc, mid + 1, e);
+    pull(n);
+  }
+  void upd(int n, int b, int e, int i, int j, long long v) {
+    push(n, b, e);
+    if (j < b || e < i)
+      return;
+    if (i <= b && e <= j) {
+      lazy[n] = v; // set lazy
+      push(n, b, e);
+      return;
+    }
+    int mid = (b + e) >> 1;
+    upd(lc, b, mid, i, j, v);
+    upd(rc, mid + 1, e, i, j, v);
+    pull(n);
+  }
+  long long query(int n, int b, int e, int i, int j) {
+    push(n, b, e);
+    if (i > e || b > j)
+      return 0; // return null
+    if (i <= b && e <= j)
+      return t[n];
+    int mid = (b + e) >> 1;
+    return combine(query(lc, b, mid, i, j), query(rc, mid + 1, e, i, j));
+  }
 };
---
+++ content/geometry/SweepLine.h
@@ -0,0 +1,5 @@
+/**
+* Author: Cp algorithm
+* Description: Finds all intersections in a set of line segments. Returns a vector of pairs of indices of line segments that intersect.
+ * Status: tested
+ */
