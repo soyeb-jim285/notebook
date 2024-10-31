d600f95c83bb7f01db1b30f4f869d0016793246d
Istahak Islam <88300613+Istahak@users.noreply.github.com>
2024-10-31 10:45:32 +0600

    Update Trie_bit.h

--- content/data-structures/Trie_bit.h
+++ content/data-structures/Trie_bit.h
@@ -76,3 +76,10 @@ struct Trie {
         delete (cur);
     }
 };
+## Next Combination Mask
+int next_combs_mask(int mask) {
+  int lsb = -mask & mask;
+  return (((mask + lsb) ^ mask) / (lsb << 2)) | (mask + lsb);
+}
+## Iterate over submask in decreasing order
+for (int s=mask; s > 0; s = (s-1)&mask) {}
