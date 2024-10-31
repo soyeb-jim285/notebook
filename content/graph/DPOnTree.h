8e354eb7b5d847968d552a195d9a9e2e51b0ebe3
Inori <103218515+Farhan1816@users.noreply.github.com>
2024-10-31 18:30:01 +0600

    Update DPOnTree.h

--- content/graph/DPOnTree.h
+++ content/graph/DPOnTree.h
@@ -2,21 +2,45 @@
  * Author: Farhan
  * Description:DPonTree
  */
-vector<array<ll, 2>> down(N), up(N);
-void dfs() {
-  // calculate down dp
+void dfs(int ind, int p)
+{
+  int tmp = 0;
+  int s=1;
+  for(auto i: adj[ind])
+  {
+    if(i!=p)
+    {
+      dfs(i, ind);
+      tmp += (down[i]+sz[i]);
+      s+=sz[i];
+    }
+  }
+  sz[ind] = s;
+  down[ind] = tmp;
 }
-void dfs2() {
-  ll pref = ? ;
-  for (auto v : adj[u]) {
-    // update up[v] and pref
+void dfs2(int ind, int p)
+{
+  if(ind!=p)up[ind] += (up[p] + n - sz[p]), up[ind]++;
+  int tmp=0;
+  for(auto i: adj[ind])
+  {
+    if(i!=p)
+    {
+      up[i] += tmp;
+      tmp += (down[i]+2LL*sz[i]);
+    }
   }
-  reverse(adj[u].begin(), adj[u].end());
-  ll suf = ? ;
-  for (auto v : adj[u]) {
-    // update up[v] and suf
+  reverse(adj[ind].begin(), adj[ind].end());
+  tmp = 0;
+  for(auto i: adj[ind])
+  {
+    if(i!=p)
+    {
+      up[i] +=tmp;
+      tmp += (down[i]+2LL*sz[i]);
+    }
   }
-  for (auto v : adj[u]) {
-    dfs2(v)
-  }
+  ans[ind] = down[ind] + up[ind];
+  for(auto i: adj[ind])if(i!=p)dfs2(i, ind);
 }
+
